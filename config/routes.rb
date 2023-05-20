e518908bf90d11cae97bbba1523d221747807852
yuta-kobayashi-dev <123618181+yuta-kobayashi-dev@users.noreply.github.com>
2023-05-20 04:42:07 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+
+  resources :post_images, only: [:new, :index, :show]
+
+  get "/homes/about" => 'homes#about', as: "about"
+
+end
